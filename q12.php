7d5aa543ba729e6fd6482db95cef10ddb3b6fae4
ytwang717 <75212722+ytwang717@users.noreply.github.com>
2020-12-11 11:47:15 +0800

    Update and rename q11.php to q12.php

--- q12.php
+++ q12.php
@@ -2,14 +2,31 @@
     <link href="style_questions.css"
           rel="stylesheet"
           type="text/css">
-    <title>Implicit Bias Question 11</title>
+    <title>Implicit Bias Question 12</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Nunito+Sans&display=swap"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Nunito+Sans&family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Nunito&family=Nunito+Sans&family=Pacifico&display=swap" rel="stylesheet">
+    <h1 align="center" class="cursive">
+        PHP Panthers
+    </h1>
+    <nav>
+        <ul>
+            <li><a href="index.php" target="_blank">Homepage</a></li>
+            <li><a href="results.php" target="_blank">Results</a></li>
+            <li><a href="reflections.php" target="_blank">Reflections</a></li>
+            <li><a href="about.php" target="_blank">About</a></li>
+            <li style="float: right;"><a>PHP Panthers</a></li>
+        </ul>
+    </nav>
 </head>
 <?php
 
 // Created by: Yutong Wang
 // Based on: index.php
 // Last Modified on: Dec 10, 2020
-// Last Modified by: Ashley Massey
+// Last Modified by: Yutong Wang
 
 // Grab the user id from the POST data sent to us from the previous page
 $user_id = $_POST["user_id"];
@@ -24,7 +41,7 @@ $question_text = "I feel more anxious talking to people of a different race than
 
 ?>
 <div calss="number">
-   <h3>11 / 11</h3>
+   <h3>12 / 12</h3>
 </div>
 <form class="content-container" method="post" action="results.php">
     <?php echo '<h1>' . $question_text . '</h1>'; ?>

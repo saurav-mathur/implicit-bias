0bd61e4cb001227011ceff89a37aee37d8426312
ytwang717 <75212722+ytwang717@users.noreply.github.com>
2020-12-11 12:00:14 +0800

    Update and rename newq4.php to q4.php

--- q4.php
+++ q4.php
@@ -40,7 +40,9 @@ $question_text = "I would have more trust in my lawyer if they were of the same 
 // Use the question text and the user id to create a form for this question that will take us to the next question OR the results page, whichever should come next:
 
 ?>
-
+<div calss="number">
+   <h3>4 / 12</h3>
+</div>
 <form class="content-container" method="post" action="q5.php">
     <?php echo '<h1>' . $question_text . '</h1>'; ?>
     <?php echo '<p><input type="hidden" name="user_id" value="' . $user_id . '" /></p>'; ?>

063d308b32529edbadbefdfced6c452fb7f9f61c
ytwang717 <75212722+ytwang717@users.noreply.github.com>
2020-12-11 12:02:20 +0800

    Update q2.php

--- q2.php
+++ q2.php
@@ -4,6 +4,23 @@
           rel="stylesheet"
           type="text/css">
     <title>Implicit Bias Question 2</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Nunito+Sans&display=swap"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Nunito+Sans&family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Nunito&family=Nunito+Sans&family=Pacifico&display=swap" rel="stylesheet">
+    <h1 align="center" class="cursive">
+        PHP Panthers
+    </h1>
+    <nav>
+        <ul>
+            <li><a href="index.php" target="_blank">Homepage</a></li>
+            <li><a href="results.php" target="_blank">Results</a></li>
+            <li><a href="reflections.php" target="_blank">Reflections</a></li>
+            <li><a href="about.php" target="_blank">About</a></li>
+            <li style="float: right;"><a>PHP Panthers</a></li>
+        </ul>
+    </nav>
 </head>
 <?php
 
@@ -25,7 +42,7 @@ $question_text = "I prefer interacting with recruiters of the same race in a car
 
 ?>
 <div calss="number">
-   <h3>2 / 11</h3>
+   <h3>2 / 12</h3>
 </div>
 
 <form class="content-container" method="post" action="q3.php">

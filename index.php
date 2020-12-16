d50cd9dc799e628861c340e14823e17d26a74904
Nirmit Jallawar <jallawar@wisc.edu>
2020-12-17 00:35:39 +0530

    homepage

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Nunito+Sans&display=swap"
           rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Nunito+Sans&family=Pacifico&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Nunito&family=Nunito+Sans&family=Pacifico&display=swap" rel="stylesheet">
-    <h1 align="center">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Nunito+Sans&family=Pacifico&display=swap"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Nunito&family=Nunito+Sans&family=Pacifico&display=swap"
+          rel="stylesheet">
+    <h1 class="topHead" align="center" >
         PHP Panthers
     </h1>
     <nav>
@@ -33,20 +35,44 @@ $user_id = rand(10000, 99999);
 <h2 align="center">
     Welcome to our Implicit Racial Bias Test!
 </h2>
-<p align = "center">We expect this survey to take about 10 minutes to
-    complete.<br> You will be asked a series of questions and will have to indicate your choice by selecting the appropriate option.
-    <br> After completing the survey, you can compare your choices to other results collected anonymously, the scale being 1 for strongly disagree and 5 for strongly agree.</p>
-<div align = "center">
-<p>Please click on the button below to start the survey:</p>
+<p align="center">
+    This test/survey is intended to allow the users to get in touch with their own (racial) biases.
+    <br>
+    This not a scientifically accurate survey - you may end up with interpretations of your answers
+    that you may not necessarily agree with.
+    <br>
+    Please note that these are just a suggested
+    interpretation of the data and should be misconstrued as anything more.
+    <br>
+    Before beginning the test, please take a moment to explore the information provided on the
+    Additional Resources page and watch the following video:
+    <br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/WLQ2OlciaVk" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+</p>
 
-<form method="post" action="q1.php">
-    <?php echo '<input type="hidden" name="user_id" value="' . $user_id . '" />'; ?>
-    <p><input type="submit" value="Begin Survey"/></p>
+<p align="center">
+    We expect this survey to take about 10 minutes to complete.
+    <br>
+    You will be asked a series of questions and will have to indicate your choice by
+    selecting the appropriate option.
+    <br>
+    Please do not second guess yourself to allow for the optimal experience.
+</p>
+<div align="center">
+    <p>Please click on the button below to start the survey:</p>
 
-</form>
+
+    <form method="post" action="q1.php">
+        <?php echo '<input type="hidden" name="user_id" value="' . $user_id . '" />'; ?>
+        <p><input type="submit" value="Begin Survey"/></p>
+
+    </form>
 </div>
 <div class="footer">
-    <p align = "center">Created by Nirmit Jallawar, Saurav Mathur, Ashley Massey and Yutong Wang for LIS 500</p>
+    <p align="center">Created by Nirmit Jallawar, Saurav Mathur, Ashley Massey and Yutong Wang for
+        LIS 500</p>
 </div>
 
 </body>

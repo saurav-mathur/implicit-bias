5b27292147bd5283c21a461c80dd33d714150cd2
aemassey <75194935+aemassey@users.noreply.github.com>
2020-12-18 00:33:35 -0600

    Update group_analysis.php

--- group_analysis.php
+++ group_analysis.php
@@ -42,7 +42,19 @@
 
 <div class="content-container">
     <div class="cent">
-        <p>Placeholder</p>
+        <p>As we can see from the results:
+         <ol>
+            <li>It's interesting that most of the Most Frequent Results are the “drastic” options (strongly agree/disagree) rather than the moderate ones. Does this indicate that our classmates have a strong attitude towards racial issues?  </li>
+            <li>The only average result and frequent result that had a greater difference than 1 was for question 1 which had to do with voting.  
+            <br>This makes us wonder what the disparity was about. 1 being the most frequent, but three being the average means it really should have ranged across the entire Likert scale while none of the other questions necessarily had to do that to achieve the same results. We still need to think more about what it is about voting and implicit racial bias that causes it to be this way. </li>
+            <li>The average result and the most frequent result was only the same on question 3 which pertained to sharing ideas with colleagues.  </li>
+            <li>Question 3 looks like it may have been neutral across the board. Is that because sharing ideas at work is not a problem (or a big problem) regardless of the race makeup of colleagues? Could it be more difficult sharing ideas with colleagues based on other identity aspects (gender, ability, etc) over race? </li>
+            <li>The average result and the most frequent result for Q11 are both the highest ones, indicating that most people are supportive of racial diversity in schools. It is encouraging to see that people have positive attitudes towards diversity.</li>
+         </ol>
+          
+            <br>Most of the results are consistent with our expectations, and from the results, we are glad that our classmates are quite opposed to racial biases. However, we also got some feedback from one of our classmates that they thought our test was meant for white people. “I think it’s very different for me as a black person to prefer a black doctor, given the history of medical racism in the US, than for a white doctor because they assume all others would be incompetent.” While we appreciated the feedback, we worded some of our questions hoping to get different responses from White People and from POC. What our classmate had to say was completely correct regardless of what we were trying to accomplish with our questions. We asked a question similar to this about lawyers as Critical Race Theory was a topic that was brought up quite a few times in one of our most used class text, <i>Algorithms of Oppression: How Search Engines Reinforce Racism</i> by Safiya Umoja Noble. While our overarching goal was not to shine a spotlight on Critical Race Theory through our questions, we used the concept to create questions that could show Critical Race Theory in practice if we were to also ask our participants to share their racial identity. 
+         <br>Overall, the implicit bias test and its results are inspiring, giving us the chance to think more deeply about racism as well as reminding ourselves of the need to be aware of and overcome our own subconscious biased views.   
+        </p>
         </div>
         </div>
 
